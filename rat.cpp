36610ea6a7a7ae5c7393cf84f074bf40563547c4
Muhammad Saim Masood <saimmasood04@gmail.com>
2026-05-31 14:13:04 -0700

    Add files via upload

---
+++ rat.cpp
@@ -0,0 +1,59 @@
+
+#include <iostream>
+using namespace std;
+
+const int N = 4;
+
+void printSolution(int sol[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << sol[i][j] << " ";
+        cout << endl;
+    }
+}
+
+bool isSafe(int maze[N][N], int x, int y) {
+    return (x >= 0 && x < N && y >= 0 && y < N && maze[x][y] == 1);
+}
+
+bool solveMazeUtil(int maze[N][N], int x, int y, int sol[N][N]) {
+    if (x == N - 1 && y == N - 1) {
+        sol[x][y] = 1;
+        return true;
+    }
+
+    if (isSafe(maze, x, y)) {
+        sol[x][y] = 1;
+
+        if (solveMazeUtil(maze, x, y + 1, sol))
+            return true;
+
+        if (solveMazeUtil(maze, x + 1, y, sol))
+            return true;
+
+        sol[x][y] = 0;
+        return false;
+    }
+    return false;
+}
+
+int main() {
+    int maze[N][N] = {
+        {1, 0, 0, 0},
+        {1, 1, 0, 1},
+        {0, 1, 0, 0},
+        {1, 1, 1, 1}
+    };
+
+    int sol[N][N] = { 0 };
+
+    if (solveMazeUtil(maze, 0, 0, sol)) {
+        cout << "Path found:" << endl;
+        printSolution(sol);
+    }
+    else {
+        cout << "No path exists." << endl;
+    }
+
+    return 0;
+}
